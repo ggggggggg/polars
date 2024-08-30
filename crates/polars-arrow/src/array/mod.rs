ac1308f54c762920e0fa02cff18f75137ef0a338
ggggggggg <ggggggggg@users.noreply.github.com>
2024-08-30 12:00:44 -0600

    progress

--- crates/polars-arrow/src/array/mod.rs
+++ crates/polars-arrow/src/array/mod.rs
@@ -24,6 +24,7 @@ use crate::bitmap::{Bitmap, MutableBitmap};
 use crate::datatypes::ArrowDataType;
 
 pub mod physical_binary;
+pub mod listview;
 
 pub trait Splitable: Sized {
     fn check_bound(&self, offset: usize) -> bool;
